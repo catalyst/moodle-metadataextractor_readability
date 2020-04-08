8427c8de2c5c5d15a719119a0bb3d82509278e03
Tom Dickman <tomdickman@catalyst-au.net>
2020-04-08 12:52:22 +1000

    :wrench: config: add missing constant import to settings page

--- settings.php
+++ settings.php
@@ -24,6 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
+require_once($CFG->dirroot . '/admin/tool/metadata/extractor/readability/constants.php');
+
 if ($hassiteconfig) {
 
     $settings->add(new admin_setting_heading('metadataextractor_readability/readabilitysettings',
